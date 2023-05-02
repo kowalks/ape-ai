86a8a0d8f712952e190c7007236cd2746e2cf7bc
Guilherme Kowalczuk <guilhermekowalczuk@gmail.com>
2023-05-02 03:46:17 -0300

    🎨 Remove linter warning in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ import versioneer
 
 if __name__ == "__main__":
     setup(
-        version=versioneer.get_version(),
-        cmdclass=versioneer.get_cmdclass(),
+        version=versioneer.get_version(), # type: ignore
+        cmdclass=versioneer.get_cmdclass(), # type: ignore
     )
